a6954026dbd17b5800055b8876229fb62f00e560
muslihudin <hikariroger@gmail.com>
2021-04-30 11:40:42 +0700

    initial story

--- story/main.go
+++ story/main.go
@@ -1,36 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 
 	rupiah "github.com/yudapc/go-rupiah"
 )
 
 func main() {
 
-	fmt.Println("Input resto name: ")
-	var name string
-	fmt.Scanln(&name)
-	fmt.Println("Input date of print: ")
-	var date string
-	fmt.Scanln(&date)
-	fmt.Println("Input cashier name: ")
-	var cashier string
-	fmt.Scanln(&cashier)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Input resto name: ")
+	scanner.Scan()
+	name := scanner.Text()
+	fmt.Print("Input date of print: ")
+	scanner.Scan()
+	date := scanner.Text()
+	fmt.Print("Input cashier name: ")
+	scanner.Scan()
+	cashier := scanner.Text()
 
 	var arritem = make(map[string]float64)
 
 	for {
-		fmt.Println("Input item (type 'exit' to finish): ")
-		var item string
-		fmt.Scanln(&item)
+		fmt.Print("Input item (type 'exit' to finish): ")
+		scanner.Scan()
+		item := scanner.Text()
 		if item == "exit" {
 			break
 		}
-		fmt.Println("Input " + item + " price: ")
-		var price float64
-		fmt.Scanln(&price)
-		arritem[item] = price
+		fmt.Print("Input " + item + " price: ")
+		scanner.Scan()
+		price := scanner.Text()
+		fprice, _ := strconv.ParseFloat(price, 64)
+		arritem[item] = fprice
 	}
 
 	fmt.Println()
